c8e09088a08f0c0c31471e1d785e44db38a70cbc
yangfei9592 <yangfei9592@163.com>
2013-04-02 23:11:36 +0800

    update

--- zoj/2850/source.py
+++ zoj/2850/source.py
@@ -2,7 +2,21 @@
 import sys
 
 
-for line in sys.stdin:
-	arr = line.split()
-	print arr
+#for line in sys.stdin:
+line = sys.stdin.readline()
+arr = line.split()
+#print "count:%d" %len(arr)
+m = int(arr[0])
+n = int(arr[1])
+while m != 0 and n != 0:
+	print "%d*%d" %(m,n)
+	for i in range(0,m):
+#		print "m=%d n=%d"%(m,n)
+		mn = sys.stdin.readline()
+		print "data:%s" %mn,
+	arr = sys.stdin.readline().split()
+	m = int(arr[0])
+	n = int(arr[1])
+
+		
 
