05416965ac3f4a62c1b5011e849f28bdb64dfb1f
yangfei9592 <yangfei9592@163.com>
2013-04-03 14:45:07 +0800

    complate2850

--- zoj/2850/source.py
+++ zoj/2850/source.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python
 import sys
 
-
-#for line in sys.stdin:
 line = sys.stdin.readline()
 arr = line.split()
-#print "count:%d" %len(arr)
-m = int(arr[0])
-n = int(arr[1])
+n = int(arr[0])
+m = int(arr[1])
 while m != 0 and n != 0:
-	print "%d*%d" %(m,n)
-	for i in range(0,m):
-#		print "m=%d n=%d"%(m,n)
-		mn = sys.stdin.readline()
-		print "data:%s" %mn,
+	matrix = [None] * n
+	for i in range(0,n):
+		matrix[i] = sys.stdin.readline().split() 
+	allCovered = True
+	for line in matrix:
+		for data in line:
+			if int(data) == 0:
+				allCovered = False
+				break
+		if not(allCovered):
+			break
+
+	mowedadjacent = False
+	if not(allCovered):
+		for i in range(0,n):
+			for j in range(0,m):
+				if int(matrix[i][j]) == 0:
+					if i > 0:
+						mowedadjacent = 0 == int(matrix[i - 1][j]) or mowedadjacent
+					if i + 1 < n:
+						mowedadjacent = 0 == int(matrix[i + 1][j]) or mowedadjacent
+					if j > 0:
+						mowedadjacent = 0 == int(matrix[i][j - 1]) or mowedadjacent
+					if j + 1 < m:
+						mowedadjacent = 0 == int(matrix[i][j + 1]) or mowedadjacent
+				if mowedadjacent:
+					break
+			if mowedadjacent:
+				break
+					
+	if not(allCovered) and not(mowedadjacent):
+		print "Yes"
+	else:
+		print "No"					
 	arr = sys.stdin.readline().split()
-	m = int(arr[0])
-	n = int(arr[1])
-
-		
-
+	n = int(arr[0])
+	m = int(arr[1])
--- zoj/2850/test.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-for x in range(1,10):
-	print "%d" %x
