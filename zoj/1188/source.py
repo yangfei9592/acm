38c9d3a4b71558ac8b020fcf996cf955302e3c43
yangfei9592 <yangfei9592@163.com>
2013-04-04 14:26:39 +0800

    problem1188

--- zoj/1188/source.py
+++ zoj/1188/source.py
@@ -3,13 +3,39 @@
 import sys
 
 testCount = int(sys.stdin.readline())
-print "testCount:%d" %testCount
+#print "testCount:%d" %testCount
+
+def unsortedness(str):
+	count = 0
+	for i in range(0,len(str)):
+		for j in range(i + 1,len(str)):
+			if str[i] > str[j]:
+				count += 1
+	return count
+
+
+
 
 for index in range(0,testCount):
 	sys.stdin.readline()
 	nm = sys.stdin.readline()
 	n = int(nm.split()[0])
 	m = int(nm.split()[1]) #line
+	DNAArr = [None] * m
 	for i in range(0,m):
-		print sys.stdin.readline(),
-	print
+		str = sys.stdin.readline().rstrip()
+		DNAArr[i] = [str,unsortedness(str)]
+	
+	for i in range(0,m):
+		for j in range(i + 1,m):
+			if DNAArr[i][1] > DNAArr[j][1]:
+				temp = DNAArr[i]
+				DNAArr[i] = DNAArr[j]
+				DNAArr[j] = temp
+	
+	for i in range(0,m):
+		print DNAArr[i][0]
+	
+	if index < testCount - 1:
+		print
+
